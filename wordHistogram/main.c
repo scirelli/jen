b1d5df1fee9e705b7d9f95fda11fea868cc7c389
kls602 <stephen.cirelli@capitalone.com>
2016-12-19 22:03:16 -0500

    Update

--- wordHistogram/main.c
+++ wordHistogram/main.c
@@ -2,28 +2,34 @@
 
 int main(int argc, char *argv[]){
     char c;
-    int newlineCount = 0;
+    int newlineCount = 0,
+        charCount = 0,
+        largestWord = 0;
 
     while((c = getchar()) != EOF){
         switch(c){
             case ' ':
-                c = '\n';
-                newlineCount = 0;
-                break;
             case '\t':
                 c = '\n';
+                printf(" (%d)", charCount);
+                if(charCount > largestWord)
+                    largestWord = charCount;
                 newlineCount = 0;
+                charCount = 0;
                 break;
             case '\n':
                 newlineCount++;
+                charCount = 0;
                 break;
             default:
-                newlineCount = 0;
+                charCount++;
                 c = '*';
+                newlineCount = 0;
         }
         if(newlineCount <= 0)
             printf("%c", c);
     }
 
+    printf("\n\nThe longest word in this text is %d characters long.\n\n", largestWord);
     return 0;
 }
